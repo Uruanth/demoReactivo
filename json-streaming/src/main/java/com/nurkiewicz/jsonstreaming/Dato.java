9e3798ff9cdeb2e09a1278e32da385683be226dd
Dairon Perilla <donperrydev@gmail.com>
2022-08-14 12:52:25 -0500

    base back

---
+++ json-streaming/src/main/java/com/nurkiewicz/jsonstreaming/Dato.java
@@ -0,0 +1,14 @@
+package com.nurkiewicz.jsonstreaming;
+
+public class Dato {
+    String id;
+    String name;
+
+    public Dato(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Dato() {
+    }
+}
---
+++ json-streaming/src/main/java/com/nurkiewicz/jsonstreaming/StreamingController.java
@@ -0,0 +1,69 @@
+package com.nurkiewicz.jsonstreaming;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import static org.springframework.http.MediaType.APPLICATION_NDJSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
+
+@CrossOrigin("*")
+@RestController
+public class StreamingController {
+
+    @GetMapping(value = "/get", produces = TEXT_EVENT_STREAM_VALUE)
+    Flux<Data> sse(@RequestParam(value = "fail", required = false, defaultValue = "false") boolean fail) {
+        return source(fail);
+    }
+
+    @GetMapping(produces = APPLICATION_NDJSON_VALUE)
+    Flux<Data> ndjson(@RequestParam(value = "fail", required = false, defaultValue = "false") boolean fail) {
+        return source(fail);
+    }
+
+    @GetMapping
+    Flux<Data> array(@RequestParam(value = "fail", required = false, defaultValue = "false") boolean fail) {
+        return source(fail);
+    }
+
+    Flux<Data> source(boolean fail) {
+        return fail ? failing() : successful();
+    }
+
+    Flux<Data> failing() {
+        return successful()
+                .concatWith(Mono.error(new RuntimeException("Opps!")));
+    }
+
+    private Flux<Data> successful() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .take(5)
+                .map(i -> new Data(i, Instant.now()));
+    }
+
+}
+
+class Data {
+    private final long seqNo;
+    private final Instant timestamp;
+//    private final String timestamp;
+
+    Data(long seqNo, Instant timestamp) {
+        this.seqNo = seqNo;
+        this.timestamp = timestamp;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public long getSeqNo() {
+        return seqNo;
+    }
+}
